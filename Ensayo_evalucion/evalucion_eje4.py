19ec9175b5a717fe2669e26cbd1196f92aa47037
pranter <pablo_maraboli@hotmail.com>
2017-05-31 19:41:50 -0400

    Agregado ensayo ejercicio 4

---
+++ Ensayo_evalucion/evalucion_eje4.py
@@ -0,0 +1,16 @@
+num1 = int(input("ingrese un numero:\n"))
+num2 = int(input("ingrese un numero:\n"))
+num3 = int(input("ingrese un numero:\n"))
+suma = 0
+suma2 = 0
+suma3 = 0
+for i in range(num1):
+    suma += num1
+
+for i in range(suma):
+    suma2 += num3
+
+for i in range(suma2):
+    suma3 += num2
+
+print (suma3)
